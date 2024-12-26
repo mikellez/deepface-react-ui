800e49db365120a10a3b31f330d9c4d57e1fa3a7
Chin Zu Yao <zuyao@Chins-MacBook-Pro.local>
2024-12-26 17:36:19 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
 
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
-  
+
   const [base64Image, setBase64Image] = useState('');
-  
+
   const [isVerified, setIsVerified] = useState(null);
   const [identity, setIdentity] = useState(null);
 
@@ -20,6 +20,8 @@ function App() {
   const [analysis, setAnalysis] = useState([]);
 
   const [facialDb, setFacialDb] = useState({});
+
+  const [isRegistered, setIsRegistered] = useState(null);
 
   useEffect(() => {
     const loadFacialDb = async () => {
@@ -31,7 +33,7 @@ function App() {
       }
       return envVarsWithPrefix;
     };
-  
+
     const fetchFacialDb = async () => {
       try {
         const loadedFacialDb = await loadFacialDb();
@@ -40,7 +42,7 @@ function App() {
         console.error('Error loading facial database:', error);
       }
     };
-  
+
     fetchFacialDb();
 
   }, [facialDb]);
@@ -70,12 +72,16 @@ function App() {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
 
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+    const targetWidth = 320; // Example target width
+    const targetHeight = 240; // Example target height
 
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    canvas.width = targetWidth;//video.videoWidth;
+    canvas.height = targetHeight;//video.videoHeight;
 
-    const base64Img = canvas.toDataURL('image/png');
+    //context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, targetWidth, targetHeight);
+
+    /*const base64Img = canvas.toDataURL('image/png');
     setBase64Image(base64Img);
 
     // first click causes blank string
@@ -90,10 +96,32 @@ function App() {
     else if (task === "analyze") {
       analyze(base64Image)
     }
-    
+    else if (task === "register") {
+      register(base64Image)
+    }*/
+
+    const imageBlob = canvas.toBlob((blob) => {
+      if (!blob) return;
+
+      console.log('size', blob.size)
+
+      // Create FormData to send image as file
+      const formData = new FormData();
+      formData.append('image', blob, 'captured_image.png'); // Name it appropriately
+
+      // Call register API to upload the image
+      if (task === "verify") {
+        verify(formData);
+      } else if (task === "analyze") {
+        analyze(formData);
+      } else if (task === "register") {
+        register(formData);
+      }
+    }, 'image/jpeg', 1);
+
   };
 
-  const verify = async (base64Image) => {
+  /*const verify = async (base64Image) => {
     try {
       for (const key in facialDb) {
         const targetEmbedding = facialDb[key];
@@ -104,15 +132,15 @@ function App() {
             detector_backend: faceDetector,
             distance_metric: distanceMetric,
             align: true,
-            img1_path: base64Image,
-            img2_path: targetEmbedding,
+            img1: base64Image,
+            img2: targetEmbedding,
             enforce_detection: false,
             anti_spoofing: antiSpoofing,
           }
         );
 
         console.log(`calling service endpoint ${serviceEndpoint}/verify`)
-  
+
         const response = await fetch(`${serviceEndpoint}/verify`, {
           method: 'POST',
           headers: {
@@ -128,7 +156,7 @@ function App() {
           setIsVerified(false);
           return
         }
-  
+
         if (data.verified === true) {
           setIsVerified(true);
           setIsAnalyzed(false);
@@ -142,23 +170,23 @@ function App() {
       if (isVerified === null) {
         setIsVerified(false);
       }
-      
+
     }
     catch (error) {
       console.error('Exception while verifying image:', error);
     }
 
-  };
+  };*/
 
   const analyze = async (base64Image) => {
     const result = []
     setIsAnalyzed(false)
-    try {  
+    try {
       const requestBody = JSON.stringify(
         {
           detector_backend: faceDetector,
           align: true,
-          img_path: base64Image,
+          img: base64Image,
           enforce_detection: false,
           anti_spoofing: antiSpoofing,
         }
@@ -179,7 +207,7 @@ function App() {
         return
       }
 
-      for (const instance of data.results){
+      for (const instance of data.results) {
         const summary = `${instance.age} years old ${instance.dominant_race} ${instance.dominant_gender} with ${instance.dominant_emotion} mood.`
         console.log(summary)
         result.push(summary)
@@ -190,13 +218,52 @@ function App() {
         setIsVerified(null);
         setAnalysis(result);
       }
-      
+
     }
     catch (error) {
       console.error('Exception while analyzing image:', error);
     }
     return result
 
+  };
+
+  const register = async (formData) => {
+    try {
+      const response = await fetch(`${serviceEndpoint}/identity/register`, {
+        method: 'POST',
+        body: formData, // Send FormData with the image
+      });
+
+      const data = await response.json();
+      if (response.status !== 201) {
+        console.log(data.error);
+        return;
+      }
+
+      setIsRegistered(true);
+    } catch (error) {
+      console.error('Exception while registering image:', error);
+    }
+  };
+
+  const verify = async (formData) => {
+    try {
+      const response = await fetch(`${serviceEndpoint}/identity/verify`, {
+        method: 'POST',
+        body: formData, // Send FormData with the image
+      });
+
+      const data = await response.json();
+      if (response.status !== 201) {
+        console.log(data.error);
+        return;
+      }
+
+      setIsVerified(true);
+    } catch (error) {
+      console.error('Exception while verifying image:', error);
+      setIsVerified(false);
+    }
   };
 
   return (
@@ -216,13 +283,15 @@ function App() {
       <header className="App-header">
         <h1>DeepFace React App</h1>
         {/* Conditional rendering based on verification status */}
+        {isRegistered === true && <p style={{ color: 'green' }}>You are registered to the system. Please verify to verify your identity.</p>}
         {isVerified === true && <p style={{ color: 'green' }}>Verified. Welcome {identity}</p>}
         {isVerified === false && <p style={{ color: 'red' }}>Not Verified</p>}
         {isAnalyzed === true && <p style={{ color: 'green' }}>{analysis.join()}</p>}
         <video ref={videoRef} style={{ width: '100%', maxWidth: '500px' }} />
         <br></br><br></br>
+        <button onClick={() => captureImage('register')}>Register</button>
         <button onClick={() => captureImage('verify')}>Verify</button>
-        <button onClick={() => captureImage('analyze')}>Analyze</button>
+        {/*<button onClick={() => captureImage('analyze')}>Analyze</button>*/}
         <br></br><br></br>
         <canvas ref={canvasRef} style={{ display: 'none' }} />
         {/*base64Image && <img src={base64Image} alt="Captured frame" />*/}
